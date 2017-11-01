723edc1141ef01ee68a1f99110944fa23abc764e
andrademicael <micaelandrade@gmail.com>
2017-11-01 13:43:12 -0300

    rotina que lê um arquivo .txt como array coluna e converte em uma matriz, no formato correto, e salva sobrescrevendo o arquivo original

--- form.py
+++ form.py
@@ -5,6 +5,6 @@ arq = 'R5_6 - N 5184.txt'
 
 m = loadtxt(arq, dtype = int)
 
-m = m.reshape((4, 24), order = 'F')
+m = m.reshape((4, 24), order = 'F') #configurar o shape da matriz
 
-savetxt(arq, m, fmt='%d' ,comments="# matriz H LDPC com taxa 5/6 e N 5184, t = 24, c = 4")
+savetxt(arq, m, fmt='%d' ,comments="# matriz H LDPC com taxa x/x e N xxx, t = xx, c = xx")
